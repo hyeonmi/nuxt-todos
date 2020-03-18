ec4561eb83bdda58c808726cb6cf56aa002462ea
hyeonmi <pilotfish22@gmail.com>
2020-03-18 13:13:11 +0900

    add store

---
+++ store/index.js
@@ -0,0 +1,69 @@
+export const state = () => ({
+  todos: [],
+  visibility: 'all',
+  todo: null
+})
+
+export const mutations = {
+  ADD_TODO (state, todo) {
+    state.todos.push(todo)
+  },
+  REMOVE_TODO (state, id) {
+    state.todos = state.todos.filter(todo => todo.id !== id)
+  },
+  EDIT_TODO (state, text) {
+    state.todo.text = text
+    const findIndex = state.todos.findIndex(todo => todo.id === state.todo.id)
+    state.todos.splice(findIndex, 1, state.todo)
+    state.todo = null
+  },
+  REMOVE_COMPLETED_TODO (state) {
+    state.todos = state.todos.filter(todo => !todo.completed)
+  },
+  UPDATE_VISIBILITY (state, visibility) {
+    state.visibility = visibility
+  },
+  SET_EDIT_TODO (state, todo) {
+    state.todo = todo
+  }
+}
+
+export const actions = {
+  createTodo ({ commit }, text) {
+    commit('ADD_TODO', {
+      id: `todo-${Date.now()}`,
+      text,
+      completed: false
+    })
+  },
+  removeTodo ({ commit }, id) {
+    commit('REMOVE_TODO', id)
+  },
+  editTodo ({ commit }, text) {
+    commit('EDIT_TODO', text)
+  },
+  removeCompletedTodo ({ commit }) {
+    commit('REMOVE_COMPLETED_TODO')
+  },
+  updateVisibility ({ commit }, visibility) {
+    commit('UPDATE_VISIBILITY', visibility)
+  },
+  settodo ({ commit }, todo) {
+    commit('SET_EDIT_TODO', todo)
+  }
+}
+export const getters = {
+  getFilteredTodos (state) {
+    const { visibility } = state
+    if (visibility === 'active') {
+      return state.todos.filter(todo => !todo.completed)
+    } else if (visibility === 'completed') {
+      return state.todos.filter(todo => todo.completed)
+    } else {
+      return state.todos
+    }
+  },
+  completedCount (state) {
+    return state.todos.filter(todo => todo.completed).length
+  }
+}
