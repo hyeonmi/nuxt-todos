f0f05422d7ac950da97010acd6bc9cc4a4f1bd8c
hyeonmi <pilotfish22@gmail.com>
2020-03-23 16:31:02 +0900

    add todo list component

---
+++ components/TodoList.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      <input :checked="todo.completed" type="checkbox" @change="toggleTodo(todo.id)">
+      <nuxt-link :to="`/todo/${todo.id}`">
+        <span @click="setTodo(todo)">{{ todo.text }}</span>
+      </nuxt-link>
+      <button type="button" @click.prevent="removeTodo(todo.id)">
+        delete
+      </button>
+      <nuxt-link :to="`/todo/edit`">
+        <button type="button" @click="setTodo(todo)">
+          edit
+        </button>
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters({
+      todos: 'getFilteredTodos'
+    })
+  },
+  beforeCreate () {
+    this.$store.dispatch('initTodos')
+  },
+  methods: {
+    ...mapActions(['removeTodo', 'toggleTodo', 'setTodo'])
+  }
+}
+</script>
+
+<style scoped>
+ul{padding:0px}
+li{list-style:none}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,18 @@
 <template>
   <div id="app">
-    <ul>
-      <li v-for="todo in todos" :key="todo.id">
-        <input :checked="todo.completed" type="checkbox" @change="toggleTodo(todo.id)">
-        <nuxt-link :to="`/todo/${todo.id}`">
-          <span @click="setTodo(todo)">{{ todo.text }}</span>
-        </nuxt-link>
-        <button type="button" @click.prevent="removeTodo(todo.id)">
-          delete
-        </button>
-        <nuxt-link :to="`/todo/edit`">
-          <button type="button" @click="setTodo(todo)">
-            edit
-          </button>
-        </nuxt-link>
-      </li>
-    </ul>
+    <todo-list />
     <todo-footer />
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
+import TodoList from '~/components/TodoList'
 import TodoFooter from '~/components/TodoFooter'
 
 export default {
   components: {
+    TodoList,
     TodoFooter
-  },
-  computed: {
-    ...mapGetters({
-      todos: 'getFilteredTodos'
-    })
-  },
-  beforeCreate () {
-    this.$store.dispatch('initTodos')
-  },
-  methods: {
-    ...mapActions(['removeTodo', 'toggleTodo', 'setTodo'])
   }
 }
 </script>
-
-<style scoped>
-ul{padding:0px}
-li{list-style:none}
-</style>
